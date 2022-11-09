1ce45008f2f24d14b3cf4e09dbc05acbe7343100
Skorp24 <110429183+skorpfiles@users.noreply.github.com>
2022-11-09 17:22:26 +0100

    refactoring

--- SkorpFiles.Memorizer.Api.Web/Controllers/RepositoryController.cs
+++ SkorpFiles.Memorizer.Api.Web/Controllers/RepositoryController.cs
@@ -31,9 +31,12 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> GetQuestionnairesAsync(GetQuestionnairesRequest request)
         {
-            var userGuid = await GetCurrentUserGuidAsync();
-            var result = await _editingLogic.GetQuestionnairesAsync(userGuid, _mapper.Map<SkorpFiles.Memorizer.Api.Models.RequestModels.GetQuestionnairesRequest>(request));
-            return Ok(_mapper.Map<GetQuestionnairesResponse>(result));
+            return await ExecuteActionToBusinessLogicAsync(async () =>
+            {
+                var userGuid = await GetCurrentUserGuidAsync();
+                var result = await _editingLogic.GetQuestionnairesAsync(userGuid, _mapper.Map<SkorpFiles.Memorizer.Api.Models.RequestModels.GetQuestionnairesRequest>(request));
+                return Ok(_mapper.Map<GetQuestionnairesResponse>(result));
+            });
         }
 
         [Route("Questionnaire/{idOrCode}", Name = "GetQuestionnaire")]
@@ -41,49 +44,10 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> GetQuestionnaireAsync(string idOrCode)
         {
-            object idOrCodeObj;
-            IdOrCode isIdOrCode;
-
-            try
-            {
-                isIdOrCode = GetIdType(idOrCode, out idOrCodeObj);
-            }
-            catch(ArgumentException)
-            {
-                return BadRequest("Unsupported ID parameter: only GUID or integer are supported.");
-            }
-
-            SkorpFiles.Memorizer.Api.Models.Questionnaire resultQuestionnaire;
-
-            try
-            {
-                if (isIdOrCode == IdOrCode.Id)
-                {
-                    resultQuestionnaire = await _editingLogic.GetQuestionnaireAsync(
-                        await GetCurrentUserGuidAsync(), (Guid)idOrCodeObj);
-                }
-                else if (isIdOrCode == IdOrCode.Code)
-                {
-                    resultQuestionnaire = await _editingLogic.GetQuestionnaireAsync(
-                        await GetCurrentUserGuidAsync(), (int)idOrCodeObj);
-                }
-                else
-                    throw new NotImplementedException("Enum method is not implemented.");
-
-                return Ok(_mapper.Map<Questionnaire>(resultQuestionnaire));
-            }
-            catch(AccessDeniedForUserException e)
-            {
-                return Unauthorized(e.Message);
-            }
-            catch(ArgumentException e)
-            {
-                return BadRequest(e.Message);
-            }
-            catch(ObjectNotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+            return await SwitchIdOrCodeAndExecuteActionToBusinessLogicAsync(idOrCode,
+                async (id) => await _editingLogic.GetQuestionnaireAsync(await GetCurrentUserGuidAsync(), id),
+                async (code) => await _editingLogic.GetQuestionnaireAsync(await GetCurrentUserGuidAsync(), code),
+                (businessLogicResult) => Ok(_mapper.Map<Questionnaire>(businessLogicResult)));
         }
 
         [Route("Questionnaire")]
@@ -91,11 +55,11 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> PutQuestionnaireAsync(PutQuestionnaireRequest request)
         {
-            try
+            return await ExecuteActionToBusinessLogicAsync(async () =>
             {
                 var creatingResult = await _editingLogic.CreateQuestionnaireAsync(await GetCurrentUserGuidAsync(), _mapper.Map<Api.Models.RequestModels.UpdateQuestionnaireRequest>(request));
                 if (creatingResult != null)
-                    return CreatedAtRoute("GetQuestionnaire",new {idOrCode=creatingResult.Code.ToString()},
+                    return CreatedAtRoute("GetQuestionnaire", new { idOrCode = creatingResult.Code.ToString() },
                     new IdentifiersGroupResponse
                     {
                         Code = creatingResult.Code,
@@ -103,19 +67,7 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
                     });
                 else
                     throw new InternalErrorException("The database hasn't returned a result.");
-            }
-            catch (AccessDeniedForUserException e)
-            {
-                return Unauthorized(e.Message);
-            }
-            catch (ArgumentException e)
-            {
-                return BadRequest(e.Message);
-            }
-            catch (ObjectNotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+            });
         }
 
         [Route("Questionnaire/{idOrCode}")]
@@ -123,47 +75,18 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> DeleteQuestionnaireAsync(string idOrCode)
         {
-            object idOrCodeObj;
-            IdOrCode isIdOrCode;
-
-            try
-            {
-                isIdOrCode = GetIdType(idOrCode, out idOrCodeObj);
-            }
-            catch (ArgumentException)
-            {
-                return BadRequest("Unsupported ID parameter: only GUID or integer are supported.");
-            }
-
-            try
-            {
-                if (isIdOrCode == IdOrCode.Id)
+            return await SwitchIdOrCodeAndExecuteActionToBusinessLogicAsync(idOrCode,
+                async (id) =>
                 {
-                    await _editingLogic.DeleteQuestionnaireAsync(
-                        await GetCurrentUserGuidAsync(), (Guid)idOrCodeObj);
-                }
-                else if (isIdOrCode == IdOrCode.Code)
+                    await _editingLogic.DeleteQuestionnaireAsync(await GetCurrentUserGuidAsync(), id);
+                    return true;
+                },
+                async (code) =>
                 {
-                    await _editingLogic.DeleteQuestionnaireAsync(
-                        await GetCurrentUserGuidAsync(), (int)idOrCodeObj);
-                }
-                else
-                    throw new NotImplementedException("Enum method is not implemented.");
-
-                return Ok();
-            }
-            catch (AccessDeniedForUserException e)
-            {
-                return Unauthorized(e.Message);
-            }
-            catch (ArgumentException e)
-            {
-                return BadRequest(e.Message);
-            }
-            catch (ObjectNotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+                    await _editingLogic.DeleteQuestionnaireAsync(await GetCurrentUserGuidAsync(), code);
+                    return true;
+                },
+                (_) => Ok());
         }
 
         [Route("Questions")]
@@ -171,24 +94,11 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> GetQuestionsAsync(GetQuestionsRequest request)
         {
-            var userGuid = await GetCurrentUserGuidAsync();
-            try
+            return await ExecuteActionToBusinessLogicAsync(async () =>
             {
-                var result = await _editingLogic.GetQuestionsAsync(userGuid, _mapper.Map<Api.Models.RequestModels.GetQuestionsRequest>(request));
+                var result = await _editingLogic.GetQuestionsAsync(await GetCurrentUserGuidAsync(), _mapper.Map<Api.Models.RequestModels.GetQuestionsRequest>(request));
                 return Ok(_mapper.Map<GetQuestionsResponse>(result));
-            }
-            catch(AccessDeniedForUserException e)
-            {
-                return Unauthorized(e.Message);
-            }
-            catch(ArgumentException e)
-            {
-                return BadRequest(e.Message);
-            }
-            catch(ObjectNotFoundException e)
-            {
-                return NotFound(e.Message);
-            }
+            });
         }
 
         private enum IdOrCode
@@ -213,6 +123,62 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
             }
             else
                 throw new ArgumentException("The value is not a GUID ID or an int code.");
+        }
+
+        private async Task<IActionResult> ExecuteActionToBusinessLogicAsync(Func<Task<IActionResult>> actionAsync)
+        {
+            IActionResult result;
+            try
+            {
+                result = await actionAsync();
+            }
+            catch (AccessDeniedForUserException e)
+            {
+                result = Unauthorized(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                result = BadRequest(e.Message);
+            }
+            catch (ObjectNotFoundException e)
+            {
+                result = NotFound(e.Message);
+            }
+            return result;
+        }
+
+        private async Task<IActionResult> SwitchIdOrCodeAndExecuteActionToBusinessLogicAsync<TBusinessLogicReturningType>(
+            string idOrCode, Func<Guid, Task<TBusinessLogicReturningType>> actionIfIdAsync, Func<int, Task<TBusinessLogicReturningType>> actionIfCodeAsync,
+            Func<TBusinessLogicReturningType, IActionResult> processResultAction)
+        {
+            object idOrCodeObj;
+            IdOrCode isIdOrCode;
+            IActionResult result;
+
+            try
+            {
+                isIdOrCode = GetIdType(idOrCode, out idOrCodeObj);
+            }
+            catch (ArgumentException)
+            {
+                return BadRequest("Unsupported ID parameter: only GUID or integer are supported.");
+            }
+
+            result = await ExecuteActionToBusinessLogicAsync(async () =>
+            {
+                TBusinessLogicReturningType businessLogicResult;
+
+                if (isIdOrCode == IdOrCode.Id)
+                    businessLogicResult = await actionIfIdAsync((Guid)idOrCodeObj);
+                else if (isIdOrCode == IdOrCode.Code)
+                    businessLogicResult = await actionIfCodeAsync((int)idOrCodeObj);
+                else
+                    throw new NotImplementedException("Enum method is not implemented.");
+
+                return processResultAction(businessLogicResult);
+            });
+
+            return result;
         }
     }
 }
