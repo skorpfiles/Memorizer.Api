9b6a0873c0887b7abb42e6834ee294477eaead81
Skorp24 <110429183+skorpfiles@users.noreply.github.com>
2023-03-17 22:13:37 +0100

    fixed mappings

--- SkorpFiles.Memorizer.Api.Web/Mapping/ApiEntitiesMappingProfile.cs
+++ SkorpFiles.Memorizer.Api.Web/Mapping/ApiEntitiesMappingProfile.cs
@@ -20,22 +20,28 @@ namespace SkorpFiles.Memorizer.Api.Web.Mapping
 
             CreateMap<SkorpFiles.Memorizer.Api.Models.Question, Question>()
                 .ForMember(dest => dest.Type, opts => opts.MapFrom(src => src.Type.ToString().PascalCaseToLowerCamelCase()))
+                .ForMember(dest => dest.Enabled, opts => opts.MapFrom(src => src.IsEnabled))
                 .IncludeAllDerived();
             CreateMap<SkorpFiles.Memorizer.Api.Models.Question, ExistingQuestion>()
-                .ForMember(dest => dest.Type, opts => opts.MapFrom(src => src.Type.ToString().PascalCaseToLowerCamelCase()));
+                .ForMember(dest => dest.Type, opts => opts.MapFrom(src => src.Type.ToString().PascalCaseToLowerCamelCase()))
+                .ForMember(dest => dest.Enabled, opts => opts.MapFrom(src => src.IsEnabled));
             CreateMap<Question, SkorpFiles.Memorizer.Api.Models.Question>()
                 .ForMember(dest => dest.Type, opts =>
                 {
                     opts.Condition(src => Enum.TryParse<QuestionType>(src.Type, out _));
                     opts.MapFrom(src => Enum.Parse<QuestionType>(src.Type!));
-                });
+                })
+                .ForMember(dest => dest.IsEnabled, opts => opts.MapFrom(src => src.Enabled));
+
             CreateMap<SkorpFiles.Memorizer.Api.Models.UserQuestionStatus, UserQuestionStatus>();
             CreateMap<SkorpFiles.Memorizer.Api.Models.TypedAnswer, TypedAnswer>();
             CreateMap<TypedAnswer, SkorpFiles.Memorizer.Api.Models.TypedAnswer>();
             CreateMap<SkorpFiles.Memorizer.Api.Models.QuestionIdentifier, QuestionIdentifier>();
             CreateMap<QuestionIdentifier, SkorpFiles.Memorizer.Api.Models.QuestionIdentifier>();
-            CreateMap<SkorpFiles.Memorizer.Api.Models.QuestionToUpdate, QuestionToUpdate>();
-            CreateMap<QuestionToUpdate, SkorpFiles.Memorizer.Api.Models.QuestionToUpdate>();
+            CreateMap<SkorpFiles.Memorizer.Api.Models.QuestionToUpdate, QuestionToUpdate>()
+                .ForMember(dest => dest.Enabled, opts => opts.MapFrom(src => src.IsEnabled));
+            CreateMap<QuestionToUpdate, SkorpFiles.Memorizer.Api.Models.QuestionToUpdate>()
+                .ForMember(dest=>dest.IsEnabled, opts=>opts.MapFrom(src=>src.Enabled));
             CreateMap<SkorpFiles.Memorizer.Api.Models.ExistingQuestion, ExistingQuestion>();
             CreateMap<ExistingQuestion, SkorpFiles.Memorizer.Api.Models.ExistingQuestion>();
             CreateMap<UserQuestionStatus, SkorpFiles.Memorizer.Api.Models.UserQuestionStatus>()
