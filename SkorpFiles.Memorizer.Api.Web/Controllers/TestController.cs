7576558d5f4132fca665e3daaf789d04de0dbdbd
Skorp24 <110429183+skorpfiles@users.noreply.github.com>
2023-05-04 18:54:53 +0200

    test

--- SkorpFiles.Memorizer.Api.Web/Controllers/TestController.cs
+++ SkorpFiles.Memorizer.Api.Web/Controllers/TestController.cs
@@ -74,7 +74,7 @@ namespace SkorpFiles.Memorizer.Api.Web.Controllers
         public IActionResult TestAzureInsights()
         {
             var config = TelemetryConfiguration.CreateDefault();
-            config.ConnectionString = "InstrumentationKey=48f82062-d2fa-469a-a4a6-e873a3dc6db9;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/";
+            config.ConnectionString = "InstrumentationKey=079ad3fb-e793-4c75-bde6-5172cac2a791;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/";
             var ai = new TelemetryClient(config);
             ai.TrackException(new NullReferenceException());
             return Ok();
