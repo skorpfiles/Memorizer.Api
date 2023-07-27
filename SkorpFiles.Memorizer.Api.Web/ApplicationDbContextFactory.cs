1fdf1ce142f3fca92c61fc73cf36ffc744a27cb5
Skorp24 <110429183+skorpfiles@users.noreply.github.com>
2023-07-27 08:55:48 +0200

    test DbContextFactory

---
+++ SkorpFiles.Memorizer.Api.Web/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using SkorpFiles.Memorizer.Api.DataAccess;
+
+namespace SkorpFiles.Memorizer.Api.Web
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer();
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
