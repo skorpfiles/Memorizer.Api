86041736fc08f74d250d4aa6cf862512602c15ba
Skorp24 <110429183+skorpfiles@users.noreply.github.com>
2024-03-09 11:16:06 +0100

    Editing Repository Test is temporary deleted

--- SkorpFiles.Memorizer.Api.DataAccess.Tests/EditingRepositoryTests.cs
+++ SkorpFiles.Memorizer.Api.DataAccess.Tests/EditingRepositoryTests.cs
@@ -23,57 +23,5 @@ namespace SkorpFiles.Memorizer.Api.DataAccess.Tests
         {
             _editingRepository = Container.Resolve<IEditingRepository>();
         }
-
-        [TestMethod]
-        [DynamicData(nameof(EditingRepositoryTestDataSource.GetQuestionnaireAsync_CorrectData_ReturnResultAsync), typeof(EditingRepositoryTestDataSource))]
-        public async Task GetQuestionnaireAsync_CorrectData_ReturnResultAsync(Questionnaire expectedQuestionnaireDb, bool useCodeInsteadOfId)
-        {
-            //Arrange
-            await RegisterUserAsync();
-
-            await DbContext.Questionnaires.AddAsync(expectedQuestionnaireDb);
-
-            await DbContext.SaveChangesAsync();
-
-            var expectedQuestionnaireMapped = Mapper.Map<Api.Models.Questionnaire>(expectedQuestionnaireDb);
-
-            //Act
-
-            Api.Models.Questionnaire actualQuestionnaire =
-                !useCodeInsteadOfId ?
-                await _editingRepository.GetQuestionnaireAsync(Constants.DefaultUserId, expectedQuestionnaireDb.QuestionnaireId,false) :
-                await _editingRepository.GetQuestionnaireAsync(Constants.DefaultUserId, expectedQuestionnaireDb.QuestionnaireCode, false);
-
-            //Assert
-            //checking to prevent lost fields
-            actualQuestionnaire.Should().BeEquivalentTo(expectedQuestionnaireMapped, opts =>
-            {
-                opts.Excluding(t => t.Id);
-                opts.Excluding(t => t.Code);
-                opts.Excluding(t => t.CountsOfQuestions);
-                return opts;
-            });
-            actualQuestionnaire.Labels.Should().BeEquivalentTo(expectedQuestionnaireMapped.Labels);
-
-            //checking fields without mapping
-            actualQuestionnaire.Id.Should().NotBeNull();
-            actualQuestionnaire.Code.Should().NotBeNull();
-            actualQuestionnaire.Name.Should().Be(expectedQuestionnaireDb.QuestionnaireName);
-            actualQuestionnaire.Availability.Should().Be(expectedQuestionnaireDb.QuestionnaireAvailability);
-            actualQuestionnaire.OwnerId.Should().Be(expectedQuestionnaireDb.OwnerId);
-            actualQuestionnaire.CountsOfQuestions.Should().Be(expectedQuestionnaireDb.Questions?.Count ?? 0);
-            actualQuestionnaire.Labels.Should().NotBeNull();
-            actualQuestionnaire.Labels!.Count.Should().Be(expectedQuestionnaireDb.LabelsForQuestionnaire!.Count);
-            foreach(var expectedLabel in expectedQuestionnaireDb.LabelsForQuestionnaire)
-            {
-                var actualLabel = actualQuestionnaire.Labels.FirstOrDefault(el => el.Name == expectedLabel.Label!.LabelName);
-                actualLabel.Should().NotBeNull();
-                actualLabel!.Id.Should().NotBeNull();
-                actualLabel!.Name.Should().Be(expectedLabel.Label!.LabelName);
-                actualLabel!.StatusInQuestionnaire.Should().NotBeNull();
-                actualLabel!.StatusInQuestionnaire!.Number.Should().Be(expectedLabel.LabelNumber);
-                actualLabel!.StatusInQuestionnaire!.ParentLabelId.Should().Be(expectedLabel.ParentLabelId);
-            }
-        }
     }
 }
