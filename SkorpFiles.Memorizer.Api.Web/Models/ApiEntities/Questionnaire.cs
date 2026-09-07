cdb5cdefaeb9d565377330a5abdd0f5b19780c9c
SkorP Files <110429183+skorpfiles@users.noreply.github.com>
2026-09-07 20:16:12 +0200

    fixed property name

--- SkorpFiles.Memorizer.Api.Web/Models/ApiEntities/Questionnaire.cs
+++ SkorpFiles.Memorizer.Api.Web/Models/ApiEntities/Questionnaire.cs
@@ -10,6 +10,6 @@
         public string? OwnerName { get; set; }
         public QuestionsCounts? CountsOfQuestions { get; set; }
         public long TotalTrainingTimeSeconds { get; set; }
-        public IEnumerable<string> LabelsInQuestionnaire { get; set; } = [];
+        public IEnumerable<string> LabelsForQuestionnaire { get; set; } = [];
     }
 }
